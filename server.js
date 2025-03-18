0b780b4cf9a31f51cdada610cf799a323d3b095f
EnricoModz <160529801+EnricoModz@users.noreply.github.com>
2025-03-18 08:42:54 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,17 @@
+//configurar nosso Back End
+
+const express = require('express')
+
+//Instancia
+
+const app = express()
+
+//Rota para front end
+
+app.use(express.static('public'))
+
+//Iniciar com localhost:3000
+
+app.listen(3000, () => {
+    console.log(`Servidor rodando, acesse link http://localhost:3000`)
+})
